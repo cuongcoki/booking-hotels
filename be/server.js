88f0d0577d4bb1dde7bad65622ce457201efe944
cuongcoki <trandinhcuongfpt2022@gmail.com>
2025-09-18 22:41:18 +0700

    second commit

--- be/server.js
+++ be/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
 import { clerkMiddleware } from '@clerk/express'
+import clerkWebhooks from "./controllers/clerkWebhooks.js";
 
 connectDB()
 
@@ -17,7 +18,7 @@ app.use(express.json());
 app.use(clerkMiddleware())
 
 // API to listen  to Clerk Webhooks
-app.use("/api/clerk",clerkMiddleware);
+app.use("/api/clerk",clerkWebhooks);
 
 app.get('/' ,(req,res) =>res.send("API is working"));
 
